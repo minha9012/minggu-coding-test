991883c84cc5b620cd8c8cc82a9a416629ebf9b2
minggu <minha1002@gmail.com>
2022-05-18 15:16:20 +0900

    LeetCode - Top 100 - Medium - 5. Longest Palindromic Substring (풀이중)

--- src/leetCode/medium/longestPalindromicSubstring.java
+++ src/leetCode/medium/longestPalindromicSubstring.java
@@ -9,23 +9,23 @@ import java.util.Map;
 
 /**
  * https://leetcode.com/problems/longest-palindromic-substring/
- *
+ * <p>
  * LeetCode - Top 100 - Medium - 5. Longest Palindromic Substring
- *
+ * <p>
  * 문제 설명
  * Given a string s, return the longest palindromic substring in s.
- *
- *
+ * <p>
+ * <p>
  * Example 1:
  * Input: s = "babad"
  * Output: "bab"
  * Explanation: "aba" is also a valid answer.
- *
- *
+ * <p>
+ * <p>
  * Example 2:
  * Input: s = "cbbd"
  * Output: "bb"
- *
+ * <p>
  * Constraints:
  * 1 <= s.length <= 1000
  * s consist of only digits and English letters.
@@ -36,42 +36,43 @@ public class longestPalindromicSubstring {
 
     static final String input1 = "babad"; //expect "bab" or "aba"
     static final String input2 = "cbbd"; //expect "bb"
+    static final String input3 = "aacabdkacaa"; //expect "aca"
+    static final String input4 = "bb"; //expect "bb"
+    static final String input5 = "ccc"; //expect "bb"
 
     public static void main(String[] args) {
-        System.out.println(solution(input1));
-        System.out.println(solution(input2));
-        for (int i = 0; i < 5; i++) {
-//            System.out.println(solution(RandomStringUtils.randomAlphabetic(10)));
-        }
+//        System.out.println(solution(input1));
+//        System.out.println(solution(input2));
+        System.out.println(solution(input3));
+        System.out.println(solution(input4));
+        System.out.println(solution(input5));
+//        for (int i = 0; i < 5; i++) System.out.println(solution(RandomStringUtils.randomAlphanumeric(10)));
+
     }
 
-    public static String solution(String input) {
+    public static String solution(String s) {
 
-        char startStr = '\0'; //시작문자
-        char endStr = '\0'; //종료문자
-        int betweenLength = 0;
+        char startChar = '\0'; //시작문자
+        char endChar = '\0'; //종료문자
+        int betweenLength = 0; //문자 길이
+        String answer = ""; //출력된 문자
 
-        for(int i=0;i<input.length();i++){
-            startStr = input.charAt(i);
+        for (int i = 0; i < s.length(); i++) {
+            startChar = s.charAt(i);
 
-            // index 마지막에서부터 같은 문자를 찾는다.
-            for(int j= input.length()-1; j > 0;j--){
-                endStr = input.charAt(j);
+            // 다음 문자열부터 검사
+            for (int j = i + 1; j < s.length(); j++) {
+                endChar = s.charAt(j);
 
-                System.out.println("start : " + startStr + ", end : " + endStr);
-
-                if( startStr == endStr){
-                    betweenLength = Math.max(betweenLength, j-i);
-                    System.out.println("between : " + betweenLength);
+                if (startChar == endChar && (j - i + 1) > betweenLength) {
+                    betweenLength = j - i + 1;
+                    answer = s.substring(i, j + 1);
+                    System.out.println("[i],[j] : " + i + "," + j + ", " + answer + "("+ betweenLength+")");
                     break;
                 }
-
             }
-
-            System.out.println(input.charAt(i));
-
         }
 
-        return "finish";
+        return betweenLength <= 1 ? String.valueOf(startChar) : answer;
     }
 }
